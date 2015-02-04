80b3f8485d9524c78230dfd3c0bf9e389475e679
Mark Haskamp <mark.haskamp@pinnsg.com>
2015-02-03 21:37:18 -0500

    timer_model tests for init

--- test/spec/timer_model_spec.js
+++ test/spec/timer_model_spec.js
@@ -1,8 +1,30 @@
-describe("timer_model", function() {
+describe("given timer_model", function() {
 
-  it("should be true", function() {
-    expect(true).toBeTruthy();
+  describe("when init", function() {
+    it("then should be defined", function() {
+      expect(timer_model).toBeDefined();
+    });
+
+    it("then minutes = 0", function() {
+      expect(timer_model.minutes).toEqual(0);
+    });
+
+    it("then seconds = 0", function() {
+      expect(timer_model.seconds).toEqual(0);
+    });
+
+    it("then startButtonLabel = 'Start'", function() {
+      expect(timer_model.startButtonLabel).toEqual("Start");
+    });
+
+    it("then timerRunning = false", function() {
+      expect(timer_model.timerRunning).toEqual(false);
+    });
   });
+
+  // describe("given handleTick()", function() {
+  //   describe("when
+  // });
 
 });
 
