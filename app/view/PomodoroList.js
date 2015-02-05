909c656172fc7a8af2884c182b943fd2796f5e8e
Mark Haskamp <mark.haskamp@pinnsg.com>
2015-02-04 19:45:35 -0500

    rendering placeholder for PomodoroList

---
+++ app/view/PomodoroList.js
@@ -0,0 +1,20 @@
+
+var PomodoroList = React.createClass({
+  getInitialState: function() {
+    return timer_model;
+  },
+
+  render: function() {
+    return (
+      <div>
+      <hr />
+      <h3>eddie would go</h3>
+      < hr/>
+      </div>
+      );
+  }
+});
+
+React.render(<PomodoroList />, document.getElementById('pomodoroList'));
+
+
